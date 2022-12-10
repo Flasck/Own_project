703cdaa2361bcb3388fa92751b6677be86111891
Lapxi <Lapxi010@yandex.ru>
2022-12-10 21:26:57 +0300

    сделал modal

--- src/components/ModalWindowContact/ModalWindowContact.jsx
+++ src/components/ModalWindowContact/ModalWindowContact.jsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
 import styles from './ModalWindowContact.module.css'
 import {useForm} from 'react-hook-form'
-import closeIcon from '@images/ModalWindowContact/closeIcon.svg'
+import closeIcon from './closeIcon.svg'
 export const ModalWindowContact = ({active, setActive}) => {
     const {register, formState: {errors, isValid}, handleSubmit, reset} = useForm({mode: 'onBlur'})
+    const {check, setb} = useState(false)
     const onSubmit = (data) => {
         alert(JSON.stringify(data))
         reset()
@@ -23,30 +24,55 @@ export const ModalWindowContact = ({active, setActive}) => {
                             required: 'Имя должно быть введено!',
                             minLength: {
                                 value: 3,
-                                message: 'Минимум 3 символа.'
+                                message: 'Минимум 3 символа!'
                             }
                         })} className={styles.modal__form__block__input} type="text"/>
-                    </div>
-                    <div className={styles.modal__form__block__error}>
-                        {errors?.Name && <p>{errors?.Name.message || 'Error'}</p>}
+                        <div className={styles.modal__form__block__error}>
+                            {errors?.Name && <p>{errors?.Name.message || 'Error'}</p>}
+                        </div>
                     </div>
                     <div className={styles.modal__form__block}>
                         <label className={styles.modal__form__block__label}>Ваш Email</label>
                         <input {...register('Email', {
                             required: 'Email должен быть веден!',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Email некоректен!"
+                            }
                         })} className={styles.modal__form__block__input} type="text"/>
-                    </div>
-                    <div className={styles.modal__form__block__error}>
-                        {errors?.Email && <p>{errors?.Email.message || 'Error'}</p>}
+                        <div className={styles.modal__form__block__error}>
+                            {errors?.Email && <p>{errors?.Email.message || 'Error'}</p>}
+                        </div>
                     </div>
                     <div className={styles.modal__form__blockArea}>
                         <label className={styles.modal__form__block__labelArea}>Ваше сообщение</label>
-                        <textarea {...register('Message', {
-                            required: 'Email должен быть веден!',
-                        })}  className={styles.modal__form__block__textArea} name="text"/>
+                        <textarea
+                            {...register('Message',
+                                {
+                                    required: 'Message должен быть веден!',
+                                    minLength: {
+                                        value: 50,
+                                        message: "Минимум 50 символов!"
+                                    },
+                                    maxLength: {
+                                        value: 300,
+                                        message: "Максимум 300 символов!"
+                                    }
+                                })}
+                            className={styles.modal__form__block__textArea}
+                            name="Message"
+                        />
+                        <div className={styles.modal__form__block__error}>
+                            {errors?.Message && <p>{errors?.Message.message || 'Error'}</p>}
+                        </div>
                     </div>
-                    <div className={styles.modal__form__block__error}>
-                        {errors?.Message && <p>{errors?.Message.message || 'Error'}</p>}
+                    <div className={styles.modal__form__block}>
+                        <a className={styles.customCheckBox} onClick={()=>{setb(true)}}>
+                            <svg  width="26" height="20" viewBox="0 0 26 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2 9.89474L9.07143 17L24 2" stroke={check ? '#16161A' : 'none'} strokeWidth="3"/>
+                            </svg>
+                        </a>
+                        <label className={styles.modal__form__block__label}>Даю согласие на обработку персональных данных</label>
                     </div>
                     <button disabled={!isValid} type='submit' className={styles.modal__btn}>Отправить</button>
                 </form>
