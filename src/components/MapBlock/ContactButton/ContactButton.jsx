b7a479cd3b54f58d93ed5e670039565e75e99989
Mike <63655075+MixelTe@users.noreply.github.com>
2022-12-10 20:53:27 +0300

    MapBlock section

---
+++ src/components/MapBlock/ContactButton/ContactButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Button } from '../../Button/Button'
+import styles from './ContactButton.module.css'
+
+export const ContactButton = () => {
+	return <Button className={styles.root}>Связаться с нами</Button>
+}
