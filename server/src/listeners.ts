2b22bebf2caba57472bd7f064d0b94b56046c3e8
Mike <63655075+MixelTe@users.noreply.github.com>
2022-12-11 18:47:24 +0300

    improve sql query

--- server/src/listeners.ts
+++ server/src/listeners.ts
@@ -10,39 +10,27 @@ Api.addRoute("/", async q =>
 
 
 Api.addRouteSqlAll("/person",
-	`Select p1.id, p1.name, p2.description, group_concat(pt.name, ';') as technology
-	From
-		(Select personId as id, text as name
+	`Select p.id,
+		(Select text
+			From Person_Text as pt
+			Inner Join TextType as tt On pt.typeId = tt.id and tt.name = 'name'
+			Inner Join Lang as l On pt.langId = l.id and l.name = $1
+			Where pt.personId = p.id) as name,
+		(Select text
+			From Person_Text as pt
+			Inner Join TextType as tt On pt.typeId = tt.id and tt.name = 'description'
+			Inner Join Lang as l On pt.langId = l.id and l.name = $1
+			Where pt.personId = p.id) as description,
+		(Select json_group_array(t.name)
+			From Person_Technology as pt
+			Inner Join Technology as t
+			On t.id = pt.technologyId
+			Where pt.personId = p.id) as technology
+	From Person as p
+	Order by p.id
+`, [["lang", "ru"]], rows => rows.map(row => ({
+	 ...row,
+	technology: JSON.parse(row.technology),
+})));
+
 		From Person_Text as pt
-		Inner Join TextType as tt
-		On pt.typeId = tt.id
-		Where langId == (Select id From Lang Where name = $1)
-			And tt.name = 'name') p1
-	Inner Join
-		(Select personId as id, text as description
-		From Person_Text as pt
-		Inner Join TextType as tt
-		On pt.typeId = tt.id
-		Where langId == (Select id From Lang Where name = $1)
-				And tt.name = 'description') p2
-	On p1.id = p2.id
-
-	Inner Join (
-		Select pt.personId, t.name
-		From Person_Technology as pt
-		Inner Join Technology as t
-		On t.id = pt.technologyId
-		) pt
-
-	On pt.personId = p1.id
-
-	Group By p1.id
-`, [["lang", "ru"]], rows =>
-	{
-		rows.forEach(row => {
-			(<any>row["technology"]) = row["technology"].split(";");
-		});
-		return rows
-	}
-);
-
