c369268f05e408de3fcdaee1e3d907c3baa94c23
Mike <63655075+MixelTe@users.noreply.github.com>
2022-12-11 20:13:57 +0300

    formating

--- server/src/listeners.ts
+++ server/src/listeners.ts
@@ -16,18 +16,18 @@ Api.addRouteSqlAll("/person",
 	`select p.id,
 		(select text
 			from Text as t
-			inner join TextType as tt On t.typeId = tt.id and tt.name = 'personName'
-			inner join Lang as l On t.langId = l.id and l.name = $1
+			inner join TextType as tt on t.typeId = tt.id and tt.name = 'personName'
+			inner join Lang as l on t.langId = l.id and l.name = $1
 			where t.objId = p.id) as name,
 		(select text
 			from Text as t
-			inner join TextType as tt On t.typeId = tt.id and tt.name = 'personDescription'
-			inner join Lang as l On t.langId = l.id and l.name = $1
+			inner join TextType as tt on t.typeId = tt.id and tt.name = 'personDescription'
+			inner join Lang as l on t.langId = l.id and l.name = $1
 			where t.objId = p.id) as description,
 		(select json_group_array(t.name)
 			from Person_Technology as t
 			inner join Technology as t
-			On t.id = t.technologyId
+			on t.id = t.technologyId
 			where t.personId = p.id) as technology
 	from Person as p
 	order by p.id
@@ -39,15 +39,15 @@ Api.addRouteSqlAll("/person",
 Api.addRouteSqlAll("/places",
 	`select (select text
 		from Text as t
-		inner join TextType as tt On t.typeId = tt.id and l.name = $1
-		inner join Lang as l On t.langId = l.id and tt.name = 'personName'
+		inner join TextType as tt on t.typeId = tt.id and l.name = $1
+		inner join Lang as l on t.langId = l.id and tt.name = 'personName'
 		where t.objId = p.personId) as person,
 		json_group_array(
 			(json_object('address',
 				(select text
 				from Text as t
-				inner join TextType as tt On t.typeId = tt.id and l.name = $1
-				inner join Lang as l On t.langId = l.id and tt.name = 'address'
+				inner join TextType as tt on t.typeId = tt.id and l.name = $1
+				inner join Lang as l on t.langId = l.id and tt.name = 'address'
 				where t.objId = p.id),
 			'coords', coords))) as places
 	from Place as p
