4898c38c75584be40f502e2374a0c62f3552ad9e
Mike <63655075+MixelTe@users.noreply.github.com>
2022-12-11 17:22:53 +0300

    rename tables

--- server/src/listeners.ts
+++ server/src/listeners.ts
@@ -4,39 +4,39 @@ import { Api } from "./api";
 Api.addRoute("/", async q =>
 {
 	return {
-		"/people": [{ id: "number", name: "string", description: "string", technologies: "string[]" }],
+		"/person": [{ id: "number", name: "string", description: "string", technologies: "string[]" }],
 	};
 }, true);
 
 
-Api.addRouteSqlAll("/people",
-	`SELECT p1.id, p1.name, p2.description, group_concat(pt.name, ';') as technology
-	FROM
-		(SELECT people as id, text as name
-		FROM People_Text as pt
-		INNER JOIN TextType as tt
-		ON pt.type = tt.id
-		WHERE lang == (Select id From Lang Where name = $1)
-			AND tt.name = 'name') p1
-	INNER JOIN
-		(SELECT people as id, text as description
-		FROM People_Text as pt
-		INNER JOIN TextType as tt
-		ON pt.type = tt.id
-		WHERE lang == (Select id From Lang Where name = $1)
-				AND tt.name = 'description') p2
-	ON p1.id = p2.id
+Api.addRouteSqlAll("/person",
+	`Select p1.id, p1.name, p2.description, group_concat(pt.name, ';') as technology
+	From
+		(Select personId as id, text as name
+		From Person_Text as pt
+		Inner Join TextType as tt
+		On pt.typeId = tt.id
+		Where langId == (Select id From Lang Where name = $1)
+			And tt.name = 'name') p1
+	Inner Join
+		(Select personId as id, text as description
+		From Person_Text as pt
+		Inner Join TextType as tt
+		On pt.typeId = tt.id
+		Where langId == (Select id From Lang Where name = $1)
+				And tt.name = 'description') p2
+	On p1.id = p2.id
 
-	INNER JOIN (
-		Select pt.people, t.name
-		From People_Technology as pt
-		INNER JOIN Technology as t
-		ON t.id = pt.technology
+	Inner Join (
+		Select pt.personId, t.name
+		From Person_Technology as pt
+		Inner Join Technology as t
+		On t.id = pt.technologyId
 		) pt
 
-	ON pt.people = p1.id
+	On pt.personId = p1.id
 
-	GROUP BY p1.id
+	Group By p1.id
 `, [["lang", "ru"]], rows =>
 	{
 		rows.forEach(row => {
