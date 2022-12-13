73e53a33e99186f9daf266a4db408930de75988e
daria <dstarichkova@gmail.com>
2022-12-14 00:21:54 +0300

    подключен бд для проектов

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,26 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styles from "./ProjectCard.module.css"
 import img from "./image.svg"
 import { FramedText } from "../FramedText/FramedText"
 
-
-export const ProjectCard = ({ data }) =>
-	<article className={styles.root}>
+export const ProjectCard = ({project}) => 
+{
+	return <article className={styles.root}>
 		<figure>
 			<img className={styles.img} src={img} alt="Не удалось загрузить фото" />
 			<figcaption>
-				<time className={styles.time}>{data.date}</time>
+				<time className={styles.time}>{project.date}</time>
 			</figcaption>
 		</figure>
-		<h2 className={styles.title}>{data.title}</h2>
+		<h2 className={styles.title}>{project.title}</h2>
 		<div className={styles.subTitle}>
-			<span className={styles.authors}>{data.authors.join(", ")}</span>
+			<span className={styles.authors}>{project.authors.join(", ")}</span>
 			<div className={styles.dot} />
-			<span>{data.type}</span>
+			<span>{project.type}</span>
 		</div>
-		<p className={styles.description}>{data.description}</p>
+		<p className={styles.description}>{project.description}</p>
 		<div className={styles.gap}></div>
 		<div className={styles.techs}>
-			{data.technologies.map((t, i) => <FramedText key={i}>{t}</FramedText>)}
+			{project.technologies.map((t, i) => <FramedText key={i}>{t}</FramedText>)}
 		</div>
 	</article>
+}
---
+++ src/store/ProjectsSlice/LoadProjectsIfNotExist.js
@@ -0,0 +1,18 @@
+import { selectLanguage } from "../LanguageSlice/selectors"
+import { ProjectsSlice } from "./index"
+import { selectProjectsIsLoaded } from "./selectors"
+
+export const LoadProjectsIfNotExist = (dispatch, getState) =>
+{
+	const state = getState();
+	if (selectProjectsIsLoaded(state)) return;
+
+	dispatch(ProjectsSlice.actions.startLoading());
+	const lang = selectLanguage(state);
+	console.log(lang)
+
+	fetch(`http://localhost:3001/projects?lang=${lang}`)
+		.then((v) => v.json())
+		.then((data) => dispatch(ProjectsSlice.actions.successLoading({ data, lang })))
+		.catch(() => dispatch(ProjectsSlice.actions.failLoading()))
+}
