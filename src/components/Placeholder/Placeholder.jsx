eadb9351c8fde2ce9be9b6538a840ef6ae7baab5
Mike <63655075+MixelTe@users.noreply.github.com>
2022-12-13 22:07:22 +0300

    Placeholder

--- src/components/Placeholder/Placeholder.jsx
+++ src/components/Placeholder/Placeholder.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { classnames } from "@utils/classnames"
 import styles from "./Placeholder.module.css"
 
 
-export function Placeholder({ className, style, width, height, rounded, widthD, heightD, unitW, unitH })
+export function Placeholder({ className, style, width, height, widthD, heightD, unitW, unitH })
 {
+	const [text, setText] = useState("");
 	const ref = useRef();
 	useEffect(() =>
 	{
@@ -29,9 +30,39 @@ export function Placeholder({ className, style, width, height, rounded, widthD, 
 		ref.current.style.setProperty("--data-y", `${rect.top}px`);
 		ref.current.style.setProperty("--data-w", `${window.innerWidth}px`);
 		ref.current.style.setProperty("--data-h", `${window.innerHeight}px`);
+
+		type(generateText(width ? width : 20 * (height || 1)), setText);
 	}, [ref, width, height, widthD, heightD, unitW, unitH]);
 
-	return <div className={classnames(className, styles.root)} style={style}>
-		<div className={classnames(styles.inner, rounded ? styles.inner_rounded : "")} ref={ref}>#</div>
-	</div>
+	return <span className={classnames(className, styles.root)} style={style}>
+		<span className={classnames(styles.inner)} ref={ref}>{text}</span>
+	</span>
 }
+
+
+async function type(text, setText)
+{
+	for (let i = 0; i < text.length; i++)
+	{
+		setText(text.slice(0, i + 1));
+		await wait(Math.random() * 100 + 10);
+	}
+}
+async function wait(t)
+{
+	return new Promise(r => setTimeout(r, t));
+}
+
+const s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo volutpat tempor. Mauris bibendum urna eu diam ornare convallis. Suspendisse potenti. Sed tristique auctor enim, quis gravida ante viverra id. Sed a suscipit justo. Vestibulum leo dui, pulvinar tempor suscipit id, lobortis vitae quam. Vivamus orci lectus, sagittis in tellus."
+	.split(" ");
+
+function generateText(len)
+{
+	let text = "";
+	while (text.length < len)
+	{
+		text += " " + s[Math.floor(Math.random() * s.length)];
+	}
+	return text.slice(1, len - 1);
+}
+window.generateText = generateText;
