58de917a723ad9abe501fde583e595deeaac9fe9
Lapxi <Lapxi010@yandex.ru>
2022-12-13 17:42:14 +0300

    a

--- src/components/MapBlock/Map/Map.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Map.module.css'
-import map from "./map.png"
-
-export const Map = () =>
-	<div className={styles.root}>
-		<img className={styles.img} src={map} alt="Map" />
-	</div>
