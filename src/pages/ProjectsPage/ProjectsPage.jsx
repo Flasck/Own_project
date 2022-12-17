e5e06553649bcbdd13ecc972c37e4cd25fb35e25
Mike <63655075+MixelTe@users.noreply.github.com>
2022-12-17 23:03:29 +0300

    spaces

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,15 +1,16 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import styles from "./ProjectsPage.module.css"
-import {SearchBar} from "@components/SearchBar/SearchBar"
-import {ProjectCard} from "@components/ProjectCard/ProjectCard"
-import {useDispatch, useSelector} from "react-redux"
-import {selectLanguage} from "@store/LanguageSlice/selectors"
-import {LoadProjectsIfNotExist} from "@store/ProjectsSlice/LoadProjectsIfNotExist"
-import {selectProjects, selectProjectsStatus} from "@store/ProjectsSlice/selectors"
-import {Statuses} from "@utils/Statuses"
-import {Spinner} from "@components/Spinner/Spinner";
+import { SearchBar } from "@components/SearchBar/SearchBar"
+import { ProjectCard } from "@components/ProjectCard/ProjectCard"
+import { useDispatch, useSelector } from "react-redux"
+import { selectLanguage } from "@store/LanguageSlice/selectors"
+import { LoadProjectsIfNotExist } from "@store/ProjectsSlice/LoadProjectsIfNotExist"
+import { selectProjects, selectProjectsStatus } from "@store/ProjectsSlice/selectors"
+import { Statuses } from "@utils/Statuses"
+import { Spinner } from "@components/Spinner/Spinner";
 
-export const ProjectsPage = () => {
+export const ProjectsPage = () =>
+{
     const dispatch = useDispatch();
     const curLan = useSelector(selectLanguage);
     const status = useSelector(selectProjectsStatus)
@@ -17,20 +18,21 @@ export const ProjectsPage = () => {
 
     const ProjectsList = useSelector(selectProjects);
 
-    if (status === Statuses.inProgress) {
+    if (status === Statuses.inProgress)
+    {
         return <>
-            <SearchBar/>
+            <SearchBar />
             <div className={styles.wrapper_spinner}>
-                <Spinner/>
+                <Spinner />
             </div>
         </>
     }
 
     return (
         <>
-            <SearchBar/>
+            <SearchBar />
             <section className={styles.wrapper}>
-                {ProjectsList?.map((project) => <ProjectCard key={project.id} project={project}/>)}
+                {ProjectsList?.map((project) => <ProjectCard key={project.id} project={project} />)}
             </section>
         </>
     )
