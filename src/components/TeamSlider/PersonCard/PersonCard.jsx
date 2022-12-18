f56405e2df74f3a5a05936a9911aa6cebcfaa8c1
Flasck <po_6706@mail.ru>
2022-12-18 17:05:22 +0300

    slider pagination, swipes

--- src/components/TeamSlider/PersonCard/PersonCard.jsx
+++ src/components/TeamSlider/PersonCard/PersonCard.jsx
@@ -2,41 +2,23 @@ import React, { useState } from "react"
 import styles from "./PersonCard.module.css"
 import { classnames } from "@utils/classnames"
 import { FramedText } from "@components/FramedText/FramedText"
-import Dark_gh from "./img/Dark_gh-logo.svg"
-import Light_gh from "./img/Light_gh-logo.svg"
-import TG_logo from "./img/TG-logo.svg"
 
-export const PersonCard = ({ refLink, gap, person }) => {
+export const PersonCard = ({ refLink, person }) => {
 	const [side, setSide] = useState(true)
 	const change = () => setSide((v) => !v)
-	const theme = localStorage.getItem("darkTheme")
 
 	return (
-		<div onClick={change} className={styles.card} ref={refLink} style={{ margin: `0 ${gap}px` }}>
+		<div onClick={change} className={styles.card} ref={refLink}>
 			<div className={classnames(styles.back, side ? styles.back_base : styles.back_rev)}>
 				<div className={styles.main_info}>
-					<img className={styles.photo} src={Light_gh} alt="No img" />
+					<img className={styles.photo} src={`http://localhost:3001/image?id=${person.imageId}`} alt="No img" />
 					<h1 className={styles.name}>{person.name}</h1>
 					<div className={styles.links}>
-						<a
-							className={styles.link}
-							href="https://github.com/"
-							target="_blank"
-							onClick={() => {
-								change()
-							}}
-						>
+						<a className={styles.link} href={person.github} target="_blank" onClick={() => change()}>
 							<Icon_Gh />
 						</a>
-						<a
-							className={styles.link}
-							href="https://web.telegram.org/"
-							target="_blank"
-							onClick={() => {
-								change()
-							}}
-						>
-							<img className={styles.link_logo} src={TG_logo} alt="" />
+						<a className={styles.link} href={person.telegram} target="_blank" onClick={() => change()}>
+							<Icon_Tg />
 						</a>
 					</div>
 				</div>
@@ -55,17 +37,19 @@ export const PersonCard = ({ refLink, gap, person }) => {
 }
 
 const Icon_Gh = () => (
-	<svg
-		width="20"
-		height="20"
-		viewBox="0 0 20 20"
-		fill="none"
-		className={styles.link_logo}
-		xmlns="http://www.w3.org/2000/svg"
-	>
+	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" className={styles.link_logo} xmlns="http://www.w3.org/2000/svg">
 		<path
 			d="M10 0.24231C8.68678 0.24231 7.38642 0.500967 6.17317 1.00351C4.95991 1.50606 3.85752 2.24266 2.92893 3.17124C1.05357 5.04661 0 7.59014 0 10.2423C0 14.6623 2.87 18.4123 6.84 19.7423C7.34 19.8223 7.5 19.5123 7.5 19.2423V17.5523C4.73 18.1523 4.14 16.2123 4.14 16.2123C3.68 15.0523 3.03 14.7423 3.03 14.7423C2.12 14.1223 3.1 14.1423 3.1 14.1423C4.1 14.2123 4.63 15.1723 4.63 15.1723C5.5 16.6923 6.97 16.2423 7.54 16.0023C7.63 15.3523 7.89 14.9123 8.17 14.6623C5.95 14.4123 3.62 13.5523 3.62 9.74231C3.62 8.63231 4 7.74231 4.65 7.03231C4.55 6.78231 4.2 5.74231 4.75 4.39231C4.75 4.39231 5.59 4.12231 7.5 5.41231C8.29 5.19231 9.15 5.08231 10 5.08231C10.85 5.08231 11.71 5.19231 12.5 5.41231C14.41 4.12231 15.25 4.39231 15.25 4.39231C15.8 5.74231 15.45 6.78231 15.35 7.03231C16 7.74231 16.38 8.63231 16.38 9.74231C16.38 13.5623 14.04 14.4023 11.81 14.6523C12.17 14.9623 12.5 15.5723 12.5 16.5023V19.2423C12.5 19.5123 12.66 19.8323 13.17 19.7423C17.14 18.4023 20 14.6623 20 10.2423C20 8.92909 19.7413 7.62873 19.2388 6.41547C18.7362 5.20222 17.9997 4.09983 17.0711 3.17124C16.1425 2.24266 15.0401 1.50606 13.8268 1.00351C12.6136 0.500967 11.3132 0.24231 10 0.24231Z"
 			fill="var(--c-text)"
+		/>
+	</svg>
+)
+
+const Icon_Tg = () => (
+	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" className={styles.link_logo} xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM14.64 6.8C14.49 8.38 13.84 12.22 13.51 13.99C13.37 14.74 13.09 14.99 12.83 15.02C12.25 15.07 11.81 14.64 11.25 14.27C10.37 13.69 9.87 13.33 9.02 12.77C8.03 12.12 8.67 11.76 9.24 11.18C9.39 11.03 11.95 8.7 12 8.49C12.0069 8.45819 12.006 8.42517 11.9973 8.3938C11.9886 8.36244 11.9724 8.33367 11.95 8.31C11.89 8.26 11.81 8.28 11.74 8.29C11.65 8.31 10.25 9.24 7.52 11.08C7.12 11.35 6.76 11.49 6.44 11.48C6.08 11.47 5.4 11.28 4.89 11.11C4.26 10.91 3.77 10.8 3.81 10.45C3.83 10.27 4.08 10.09 4.55 9.9C7.47 8.63 9.41 7.79 10.38 7.39C13.16 6.23 13.73 6.03 14.11 6.03C14.19 6.03 14.38 6.05 14.5 6.15C14.6 6.23 14.63 6.34 14.64 6.42C14.63 6.48 14.65 6.66 14.64 6.8Z"
+			fill="#33A8E4"
 		/>
 	</svg>
 )
--- src/components/TeamSlider/TeamSlider.jsx
+++ src/components/TeamSlider/TeamSlider.jsx
@@ -9,101 +9,121 @@ import { LoadPeopleIfNotExist } from "@store/PeopleSlice/LoadPeopleIfNotExist"
 import { Placeholder } from "../Placeholder/Placeholder"
 import { classnames } from "../../utils/classnames"
 
+var params = { gap: 0, step: 0, slide: 0, initialOffset: 0, initTouch: 0, lastTouch: 0, threshold: 0 }
+
 export const TeamSlider = () => {
 	const dispatch = useDispatch()
 	const curLan = useSelector(selectLanguage)
 	useEffect(() => dispatch(LoadPeopleIfNotExist), [curLan])
 	const PeopleList = useSelector(selectPeople)
 
+	// Состояние сдвига для рендера
+	const [offset, setOffset] = useState(0)
+
 	// Ссылки для снятия ширины
 	const wrapperRefWidth = useRef(null)
 	const cardRefWidth = useRef(null)
 
-	// Состояния для всех измерений
-	const [gap, setGap] = useState(0)
-	const [step, setStep] = useState(0)
-	const [slide, setSlide] = useState(0)
-	const [offset, setOffset] = useState(0)
-
 	// Функции переключения между слайдами
 	const prev_slide = () => {
-		if (slide > 0) {
-			setSlide((state) => state - 1)
-			setOffset((state) => state + step)
+		if (params.slide > 0) {
+			params.slide -= 1
+			reRender()
 		}
 	}
 	const next_slide = () => {
-		if (slide < PeopleList.length - 1) {
-			setSlide((state) => state + 1)
-			setOffset((state) => state - step)
+		if (params.slide < PeopleList.length - 1) {
+			params.slide += 1
+			reRender()
+		}
+	}
+	// Функции свайпов
+	const touchStart = (event) => {
+		params.initTouch = event.touches[0].clientX
+		params.lastTouch = event.touches[0].clientX
+	}
+
+	const touchMove = (event) => {
+		params.lastTouch = event.touches[0].clientX
+	}
+
+	const endTouch = (event) => {
+		params.lastTouch - params.initTouch > params.threshold ? prev_slide() : null
+		params.lastTouch - params.initTouch < -params.threshold ? next_slide() : null
+	}
+
+	// Функция перерасчета параметров
+	const resize = () => {
+		if (wrapperRefWidth.current && cardRefWidth.current) {
+			params.gap = (wrapperRefWidth.current.offsetWidth - cardRefWidth.current.offsetWidth) / 2 - cardRefWidth.current.offsetWidth / 3
+			params.step = cardRefWidth.current.offsetWidth + params.gap
+			params.initialOffset = (wrapperRefWidth.current.offsetWidth - cardRefWidth.current.offsetWidth) / 2
+			params.threshold = wrapperRefWidth.current.offsetWidth / 10
+			reRender()
 		}
 	}
 
-	// Получаем изначальные измерения
+	// Функция перерендеринга
+	const reRender = () => setOffset(params.initialOffset - params.slide * params.step)
+
 	useLayoutEffect(() => {
 		if (wrapperRefWidth.current && cardRefWidth.current) {
-			setGap(
-				(wrapperRefWidth.current.offsetWidth - cardRefWidth.current.offsetWidth) / 4 -
-					cardRefWidth.current.offsetWidth / 6
-			)
-			setStep(
-				(cardRefWidth.current.offsetWidth * 2) / 3 +
-					(wrapperRefWidth.current.offsetWidth - cardRefWidth.current.offsetWidth) / 2
-			)
-			setOffset((wrapperRefWidth.current.offsetWidth - cardRefWidth.current.offsetWidth) / 2)
-			setSlide(0)
+			// Получаем изначальные измерения
+			resize()
+
+			// Слушатели нажатий
+			document.getElementById("slider").addEventListener("touchstart", touchStart)
+			document.getElementById("slider").addEventListener("touchmove", touchMove)
+			document.getElementById("slider").addEventListener("touchend", endTouch)
+
+			// Слушатели кликов
+			document.getElementById("slider").addEventListener("mousedown", touchStart)
+			document.getElementById("slider").addEventListener("mousemove", touchMove)
+			document.getElementById("slider").addEventListener("mouseup", endTouch)
+
+			// Слушатель изменений окна
+			window.addEventListener("resize", resize)
+
+			// Навигация по клавишам
+			document.onkeyup = () => {
+				var e = e || window.event
+				e.which == 37 ? prev_slide() : null
+				e.which == 39 ? next_slide() : null
+			}
 		}
 	}, [PeopleList])
-
-	// Следим за изменениями
-	window.addEventListener("resize", () => {
-		if (wrapperRefWidth.current && cardRefWidth.current) {
-			setGap(
-				(wrapperRefWidth.current.offsetWidth - cardRefWidth.current.offsetWidth) / 4 -
-					cardRefWidth.current.offsetWidth / 6
-			)
-			setStep(
-				(cardRefWidth.current.offsetWidth * 2) / 3 +
-					(wrapperRefWidth.current.offsetWidth - cardRefWidth.current.offsetWidth) / 2
-			)
-			setOffset((wrapperRefWidth.current.offsetWidth - cardRefWidth.current.offsetWidth) / 2)
-			setSlide(0)
-			console.log("Resized") // Заходит сюда 3 раза
-		}
-	})
-
-	document.onkeyup = () => {
-		var e = e || window.event
-		if (e.which == 37) {
-			prev_slide()
-		}
-		if (e.which == 39) {
-			next_slide()
-		}
-	}
 
 	return (
 		<>
 			{PeopleList ? (
-				<div className={styles.slider}>
+				<div className={styles.slider} id="slider">
 					<div className={styles.slider_overflow} ref={wrapperRefWidth}>
-						<div className={styles.slides_line} style={{ margin: `0 ${-gap}px`, left: `${offset}px` }}>
+						<div className={styles.slides_line} style={{ gap: `${params.gap}px`, transform: `translate3d(${offset}px, 0, 0)` }}>
 							{PeopleList.map((person) => (
-								<PersonCard key={person.id} gap={gap} person={person} refLink={cardRefWidth} />
+								<PersonCard key={person.id} person={person} refLink={cardRefWidth} />
 							))}
 						</div>
 					</div>
-					<div
-						className={classnames(styles.btn_prev, slide == 0 ? styles.btn_prev_dis : "")}
-						onClick={prev_slide}
-					>
+					<div className={classnames(styles.btn_prev, params.slide == 0 ? styles.btn_prev_dis : "")} onClick={prev_slide}>
 						❮
 					</div>
 					<div
-						className={classnames(styles.btn_next, slide == 4 ? styles.btn_next_dis : "")}
+						className={classnames(styles.btn_next, params.slide == PeopleList.length - 1 ? styles.btn_next_dis : "")}
 						onClick={next_slide}
 					>
 						❯
+					</div>
+					<div className={styles.dots}>
+						{PeopleList.map((el, index) => (
+							<div
+								key={Math.random() * index}
+								className={classnames(styles.dot, params.slide == index ? styles.dot_active : "")}
+								onClick={() => {
+									params.slide = index
+									reRender()
+								}}
+							></div>
+						))}
 					</div>
 				</div>
 			) : (
